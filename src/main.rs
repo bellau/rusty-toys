eac2104c4233053fbc5569a31db4bfa2f50f07b0
Laurent Belmonte <laurent.belmonte@gmail.com>
2018-02-20 11:29:34 +0100

    Day 2. "hello world" with nom parser
    My first steps with nom (4.0.0-beta1). It's a little confusing at first glance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
 
+#[macro_use]
+extern crate nom;
+
+
+use nom::{alphanumeric, space};
+
+
+
+named!(name_parser<&[u8],&str>,
+    do_parse!(
+        tag_s!("i am") >>
+        opt!(space) >>
+        v : map_res!(alphanumeric, std::str::from_utf8) >>
+        (v)
+    )
+);
+
 fn main() {
-    println!("hello world")
+    let t = &b"i am world
+    "[..];
+    let res = name_parser(t);
+    match res {
+        Ok(name) => println!("hello {}", name.1),
+        Err(error) => println!("bad format {:?}", error),
+    }
+
 }
