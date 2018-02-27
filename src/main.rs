7c8e7e580d3e3fd9d6145199dcd0fa766be61d4b
Laurent Belmonte <laurent.belmonte@gmail.com>
2018-02-27 10:43:00 +0100

    Day 5. Borrow some code from https://gitlab.com/goboom/sgf and make parser work

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,62 @@
 
+#![feature(slice_patterns, try_from, test)]
 #[macro_use]
 extern crate nom;
 
-named!(propident_parser<&[u8],&str>,
-    do_parse!(
-      v : take_while!(|ch| ch >= b'A' && ch <= b'Z') >>
-          (std::str::from_utf8(v).unwrap())
-    )
+extern crate test;
+
+mod types;
+
+use types::*;
+
+named!(propident_parser<&[u8],PropertyIdent>,
+    map!(map_res!(take_while1!(|ch| ch >= b'A' && ch <= b'Z'), std::str::from_utf8), PropertyIdent::from)
 );
 
-named!(propvalue_parser<&[u8],&str>,
-    do_parse!(
-        v : delimited!(
+named!(propvalue_parser<&[u8],String>,
+    ws!(delimited!(
             char!('['),
             map_res!(
                 escaped!(take_while1!(|chr| chr != b'"' && chr != b']' && chr != b'\\'), '\\', one_of!("]\\:")),
-                std::str::from_utf8
+                |s:&[u8]| (String::from_utf8(s.to_vec()))
             ),
             char!(']')
-        ) >>
-        (v)
+        )
     )
 );
 
-named!(property_parser<&[u8], (&str, Vec<&str>)>,
-    ws!(do_parse!(
-        id : propident_parser >>
-        v :   many0!(propvalue_parser) >>
-        ((id,v))
-    ))
+named!(property_parser<&[u8],Property>,
+    ws!(
+        map_res!(
+             do_parse!(
+        id  :   propident_parser >>
+        v   :   many1!(propvalue_parser) >>
+        (id,v)), 
+        |(id,v)| Property::try_new(id,v))
+    )
 );
 
-named!(node_parser<&[u8],Vec<(&str,Vec<&str>)>>,
+named!(node_parser<&[u8],Node>,
     ws!(do_parse!(
         tag!(";") >>
-        v : many0!(property_parser) >>
+        v : many1!(property_parser) >>
         (v)
     ))
 );
 
-#[derive(Debug)]
-struct GameTree<'a> {
-  nodes: Vec<Vec<(&'a str, Vec<&'a str>)>>,
-  gametrees: Vec<GameTree<'a>>,
-}
-
-
-named!(sequence_parser<&[u8],Vec<Vec<(&str,Vec<&str>)>>>,
-    many0!(node_parser)
-);
-
 named!(gametree_parser<&[u8],GameTree>,
-    do_parse!(
+    ws!(do_parse!(
      tag!("(") >>
       nodes : many0!(node_parser) >>
       gametrees : many0!(gametree_parser) >>
       tag!(")") >>
       (GameTree{ nodes : nodes ,gametrees : gametrees})
-    )
+    ))
 );
 
 fn main() {
   let t = &b";AB[1][2] CD[4][3]\0"[..];
-  let res = sequence_parser(t);
+  let res = node_parser(t);
   match res {
     Ok((_, v)) => println!("hello {:?}",v),
     Err(error) => println!("bad format {:?}", error),
@@ -75,7 +69,7 @@ mod tests {
   #[test]
   fn test_propident_parser() {
     let propident_res = ::propident_parser(&b"AB\0"[..]);
-    assert_eq!(propident_res.unwrap().1, "AB");
+    assert_eq!(propident_res.unwrap().1, ::PropertyIdent::from("AB"));
   }
 
   #[test]
@@ -88,14 +82,27 @@ mod tests {
   fn test_property_parser() {
     let prop_res = ::property_parser(&b"AB[12] [33] [f\\]jfj]\0"[..]);
     assert_eq!(prop_res.unwrap().1,
-        ("AB",vec!("12","33","f\\]jfj")));
+    ::Property::Unknown(String::from("AB"),vec!(String::from("12"),String::from("33"),String::from("f\\]jfj"))));
   }
 
   #[test]
   fn test_node_parser() {
     let prop_res = ::node_parser(&b";AB[12][bb]CD[aa]\0"[..]);
     assert_eq!(prop_res.unwrap().1,
-        vec!( ("AB",vec!("12","bb")), ("CD", vec!("aa"))));
+        vec!( 
+            ::Property::Unknown(String::from("AB"),vec!(String::from("12"),String::from("bb"))),
+            ::Property::Unknown(String::from("CD"),vec!(String::from("aa")))));
+  }
+
+  #[test]
+  fn test_gametree_parser() {
+    let gametree_res = ::gametree_parser(&b"(;AB[12][bb]CD[aa])\0"[..]);
+    assert_eq!(gametree_res.unwrap().1,
+        ::GameTree { nodes : 
+         vec!(vec!( 
+            ::Property::Unknown(String::from("AB"),vec!(String::from("12"),String::from("bb"))),
+            ::Property::Unknown(String::from("CD"),vec!(String::from("aa")))))
+        , gametrees : vec!()});
   }
 
 }
---
+++ src/types.rs
@@ -0,0 +1,117 @@
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+// borrowed code from https://gitlab.com/goboom/sgf
+#[derive(Debug)]
+pub enum SgfParseError {
+    GeneralParseError,
+    IncompleteInput,
+    InvalidMoveCoordinate(String),
+}
+
+#[derive(PartialEq,Debug)]
+pub struct GameTree {
+  pub nodes: Vec<Node>,
+  pub gametrees: Vec<GameTree>,
+}
+
+pub type Node = Vec<Property>;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Color {
+    Black,
+    White,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Coordinate(pub usize, pub usize);
+
+#[derive(Debug, PartialEq)]
+pub enum Property {
+    Move(Color, Coordinate),
+    Pass(Color),
+    Comment(String),
+    Unknown(String, Vec<String>),
+}
+
+impl Property {
+    pub(crate) fn try_new(id: PropertyIdent, args: Vec<String>) -> Result<Self, SgfParseError> {
+        match id {
+            PropertyIdent::Black => {
+                let color = Color::Black;
+                // TODO: Coordinates tt are also considered a pass on boards <= 19x19
+                if args.len() != 1 {
+                    return Err(SgfParseError::GeneralParseError);
+                }
+                let coord = &args[0][..];
+                if coord.is_empty() {
+                    Ok(Property::Pass(color))
+                } else {
+                    Ok(Property::Move(color, coord.try_into()?))
+                }
+            }
+            PropertyIdent::White => {
+                let color = Color::White;
+                if args.len() != 1 {
+                    return Err(SgfParseError::GeneralParseError);
+                }
+                let coord = &args[0][..];
+                if coord.is_empty() {
+                    Ok(Property::Pass(color))
+                } else {
+                    Ok(Property::Move(color, coord.try_into()?))
+                }
+            }
+            PropertyIdent::Comment => Ok(Property::Comment(args[0].to_owned())),
+            PropertyIdent::Unknown(id_str) => Ok(Property::Unknown(id_str, args)),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum PropertyIdent {
+    White,
+    Black,
+    Comment,
+    Unknown(String),
+}
+
+impl<S> From<S> for PropertyIdent
+where
+    S: Into<String>,
+{
+    fn from(s: S) -> PropertyIdent {
+        let s = s.into();
+        match s.as_ref() {
+            "W" => PropertyIdent::White,
+            "B" => PropertyIdent::Black,
+            "C" => PropertyIdent::Comment,
+            _ => PropertyIdent::Unknown(s.to_owned()),
+        }
+    }
+}
+
+fn char_code_to_coordinate(code: u8) -> Option<usize> {
+    if (b'a' <= code) && (b'z' >= code) {
+        Some((code - b'a') as usize)
+    } else {
+        None
+    }
+}
+
+impl<'a> TryFrom<&'a str> for Coordinate {
+    type Error = SgfParseError;
+    fn try_from(s: &'a str) -> Result<Coordinate, SgfParseError> {
+        match *s.as_bytes() {
+            [x, y] => {
+                if let (Some(x), Some(y)) = (char_code_to_coordinate(x), char_code_to_coordinate(y))
+                {
+                    Ok(Coordinate(x, y))
+                } else {
+                    Err(SgfParseError::InvalidMoveCoordinate(s.to_owned()))
+                }
+            }
+            [] | [_..] => Err(SgfParseError::InvalidMoveCoordinate(s.to_owned())),
+        }
+    }
+}
